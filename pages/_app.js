92a752e76cecb8b767ef00ec0d56feabab02ca8f
Jaseempk <jasimpk011@gmail.com>
2023-11-10 12:02:54 +0530

    ahad

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import{Provider} from "@self.id/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+
+  <Provider client={{ceramic:"testnet-clay"}}>
+    return <Component {...pageProps} />
+  </Provider>
+  )
 }
